6f5e3d08835dd53a9f91f875be23a3dfec41dfcf
Rajat Monga <rajatmonga@Rajats-MacBook-Pro.local>
2017-12-27 17:49:09 -0600

    refactored for legibility and maintainability

--- CircularLoaderBTA/ViewController.swift
+++ CircularLoaderBTA/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDownloadDelegate {
     
-    let shapeLayer = CAShapeLayer()
+    var shapeLayer: CAShapeLayer!
     var pulsatingLayer: CAShapeLayer!
     
     let percentageLabel : UILabel = {
@@ -34,57 +34,55 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         animatePulsatingLayer()
     }
     
+    private func createCircleShapeLayer (strokeColor: UIColor, fillColor: UIColor, lineWidth: CGFloat) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = lineWidth
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = kCALineCapRound
+        layer.position = view.center
+        
+        return layer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNotificationObservers()
         view.backgroundColor = UIColor.backgroundColor
         
+        setupCircleLayers()
         
-        //let center = view.center
-        let trackLayer = CAShapeLayer()
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        
-        pulsatingLayer = CAShapeLayer()
-        pulsatingLayer.path = circularPath.cgPath
-        
-        pulsatingLayer.strokeColor = UIColor.clear.cgColor
-        pulsatingLayer.lineWidth = 10
-        pulsatingLayer.fillColor = UIColor.pulastingFillColor.cgColor
-        pulsatingLayer.lineCap = kCALineCapRound
-        pulsatingLayer.position = view.center
-        view.layer.addSublayer(pulsatingLayer)
-        
-        animatePulsatingLayer()
-        
-        trackLayer.path = circularPath.cgPath
-
-        trackLayer.strokeColor = UIColor.trackStrokeColor.cgColor
-        trackLayer.lineWidth = 20
-        trackLayer.fillColor = UIColor.backgroundColor.cgColor
-        trackLayer.lineCap = kCALineCapRound
-        trackLayer.position = view.center
-        
-        view.layer.addSublayer(trackLayer)
-        
-        
-        
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.outlineStrokeColor.cgColor
-        shapeLayer.lineWidth = 20
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineCap = kCALineCapRound
-        shapeLayer.strokeEnd = 0
-        shapeLayer.position = view.center
-        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi/2, 0, 0, 1)
-        
-        view.layer.addSublayer(shapeLayer)
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action:  #selector(handleTap)))
         
+        
+        setupPercentageLabel()
+    }
+    
+    
+    private func setupCircleLayers() {
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: UIColor.pulastingFillColor, lineWidth: 10)
+        view.layer.addSublayer(pulsatingLayer)
+        
+        
+        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor, lineWidth: 20)
+        view.layer.addSublayer(trackLayer)
+        animatePulsatingLayer()
+        
+        shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear, lineWidth: 20)
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi/2, 0, 0, 1)
+        
+        shapeLayer.strokeEnd = 0
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func setupPercentageLabel() {
         view.addSubview(percentageLabel)
         percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         percentageLabel.center = view.center
-        
     }
     
     let urlString = "https://i.imgur.com/TNL8Otc.jpg"
